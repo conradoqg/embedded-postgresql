cfa54597cffb7a6483e4bc9e716356346ade1d11
Conrado Quilles Gomes <conradoqg@gmail.com>
2021-08-29 22:11:11 -0300

    Fix module exported class name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import EmbeddedPostgreSQL, { PostgresConfig } from './EmbeddedPostgreSQL';
 import { install, uninstall, checkInstallation, defaultInstallPath } from './installer';
 
-export { EmbeddedPostgreSQL as EmbeddedPostgres, PostgresConfig, install, uninstall, checkInstallation, defaultInstallPath };
+export { EmbeddedPostgreSQL, PostgresConfig, install, uninstall, checkInstallation, defaultInstallPath };
